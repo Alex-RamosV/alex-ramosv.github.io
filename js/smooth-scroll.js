5df4d86789d6bbeed08e55282d3173af83ecfeb4
AlexRamos_ <arv_lad@hotmail.com>
2026-02-09 22:02:19 -0600

    Massive Update - Part 02

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,34 +1,43 @@
 // Select all links with hashes
 $('a[href*="#"]')
-  // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
-    // On-page links
     if (
       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
       && 
       location.hostname == this.hostname
     ) {
-      // Figure out element to scroll to
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
+      
       if (target.length) {
-        // Only prevent default if animation is actually gonna happen
         event.preventDefault();
+
+        // --- LÓGICA RESPONSIVA AQUÍ ---
+        // Definimos una variable para el margen
+        var offsetVariable;
+
+        // Si el ancho de la ventana es menor a 992px (Móvil/Tablet)
+        if ($(window).width() < 992) {
+            offsetVariable = 85; // Valor para celular
+        } else {
+            offsetVariable = 95; // Valor para PC
+        }
+
+        // Calculamos la posición final usando nuestra variable
+        var posicionFinal = target.offset().top - offsetVariable;
+
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: posicionFinal
         }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
           var $target = $(target);
           $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
+          if ($target.is(":focus")) { 
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
+            $target.attr('tabindex','-1'); 
+            $target.focus(); 
           };
         });
       }
